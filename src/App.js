c74e870fc46e1bb55c3a7b5f6b55ccfdda7b9cb5
animesh <phb21013@tezu.ac.in>
2024-08-15 03:23:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
           type="text"
           value={url}
           onChange={(e) => seturl(e.target.value)}
-          placeholder="enter user id"
+          placeholder="enter Uniform Resource Locator"
           className="block w-96 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-6"
         />
         
